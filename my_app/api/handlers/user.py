edd7ed6bbcd9fbcfb2c1c784ea6cc3eec9e06aff
Dmitrey_Chernov <psevdotrotheus@gmail.com>
2026-02-08 17:05:32 +0300

    Check some errors

--- my_app/api/handlers/user.py
+++ my_app/api/handlers/user.py
@@ -8,19 +8,31 @@ from api.schemas.user import user_schema, UserSchema
 # url: /users/<int:user_id> - GET
 @app.get("/users/<int:user_id>")
 def get_user_by_id(user_id: int):
-    user = db.get_or_404(UserModel, user_id, description=_("User with id=%(user_id)s not found", user_id=user_id))
-    return user, 200
+    user = db.get_or_404(UserModel, user_id, description="Пользователь не найден")
+    return jsonify(user_schema.dump(user)), 200
 
 
 # url: /users - GET
-@app.get("/users")
+@app.route("/users")
 def get_users():
     users = db.session.scalars(db.select(UserModel)).all()
-    return users, 200
+    return jsonify(user_schema.dump(users, many=True)), 200
 
 
 # url:  /users - POST
-@app.post("/users")
-def create_user(user):
-    user.save()  
-    return user
+@app.route("/users", methods=["POST"])
+def create_user():
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({"error": "No input data"}), 400
+
+    # Валидация и десериализация через Marshmallow
+    try:
+        user = user_schema.load(json_data)
+    except Exception as err:
+        return jsonify({"error": str(err)}), 400
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(user)), 201
